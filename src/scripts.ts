6bd9c5ade4c2360a73b5f85f44af508a2a54bcc0
Arvids Rozitis <arvids.rozitis.cits@gmail.com>
2023-01-13 21:57:22 +0200

    done all tasks

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,3 +1,4 @@
+// Tikkai vienā vietā bija jāpievieno Tips
 //----------------------button1
 
 const button1 = document.querySelector(".js-btn1")
@@ -65,8 +66,6 @@ button6.addEventListener('click', () => {
     let count = 0;
     if (count !== 10) {
         button6.setAttribute('disabled','disabled');
-    } else {
-        button6.removeAttribute('disabled');
     }
 
     const intervalId = setInterval(() => {
@@ -74,11 +73,50 @@ button6.addEventListener('click', () => {
         square6Text.textContent = count.toString();
         if (count === 10) {
             clearInterval(intervalId);
+            button6.removeAttribute('disabled')
         }
-    }, 3000);
+    }, 2000);
 });
 
 //----------------------button7
 
 const button7 = document.querySelector(".js-btn7");
+const section = document.querySelector(".js-section");
+const body = document.querySelector(".js-body");
 
+button7.addEventListener('click', () => {
+    body.classList.toggle('dark-theme');
+})
+
+//----------------------hover on 5 kaut kas dažreiz lago
+
+const square5Text = document.querySelector(".js-square5-text");
+
+square5.addEventListener('mouseenter', () => {
+    let countOver = 0;    
+
+    const intervalId = setInterval(() => {
+        countOver++;
+        square5Text.textContent = countOver.toString();
+        if (countOver === 10) {
+            clearInterval(intervalId);
+        }
+    }, 500);
+    square5.addEventListener("mouseout", () => {
+        clearInterval(intervalId);
+        countOver = 0;
+        square5Text.innerHTML = countOver.toString();
+      });
+})
+
+//----------------------input in real time (atratu stack overflow)
+
+const inputField = document.querySelector(".js-input-field");
+const inputLog = document.querySelector(".js-input-log");
+
+const inputHandler = function(e: any) {
+    inputLog.innerHTML = e.target.value;
+  }
+
+inputField.addEventListener('input', inputHandler);
+inputField.addEventListener('propertychange', inputHandler);
